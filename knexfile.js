24f1aae451fa2b617487d2b06a03e4de205dd66e
mvp <markdark93@gmail.com>
2020-06-21 01:51:41 -0400

    agora vai vai

--- knexfile.js
+++ knexfile.js
@@ -54,17 +54,14 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 'postgres://qucgjjglwdklfa:bfbee71de2cf52f4f0e7426e15e283193ca3a7e9a7cb60b3a6c992565bbfbe02@ec2-34-230-231-71.compute-1.amazonaws.com:5432/d33boe74hm0svv',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations'
     }
   }
 
