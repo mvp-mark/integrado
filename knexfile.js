5cde3d55bc580114b71109c296ccabb8c4b4d5ae
mvp <markdark93@gmail.com>
2020-06-21 01:32:31 -0400

    damn

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations'
     }
   },
   // development: {
